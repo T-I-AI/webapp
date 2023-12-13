332ecb92595087b1c8278133d3d3c50d1f2bdaf1
siteshbiswal <siteshbiswal35@gmail.com>
2023-12-13 13:16:07 +0530

    commit folder to repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import parallaxImage from './logo.png';
+import parallaxImage1 from './logo.png';
+import logo from './logo.png';
+import icon from './icon.png';
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="header">
+        <div id="navbar">
+          <img src={logo} className="logo" alt="logo" />
+          <img src={icon} className="icon" alt="icon" />
+          <div id="navbar-left">
+            <a href="research.jsx">Research</a>
+            <a href="products.jsx">Products</a>
+            <a href="company.jsx">Company</a>
+            <a href="#search">Search</a>
+          </div>
+          <div id="navbar-right">
+            <a href="#signup" id="signup">Sign Up</a>
+            <a href="#login" id="login">Log In</a>
+          </div>
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Impossible AI
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="parallax" style={{backgroundImage : `url(${parallaxImage})`}}></div>
+      <div style={{ height: '500px', fontSize: '30px' }}>
+        "Impossibility is the True Intelligence"
+      </div>
+      <div className="parallax1" style={{backgroundImage : `url(${parallaxImage1})`}}></div>
+      <div style={{ height: '800px', fontSize: '30px' }}>
+        "Impossibilty is the True Intelligence"
+      </div>
+
     </div>
   );
 }
